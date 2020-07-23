01fcc22e6ab741d42ad297de48ac66a7b1fd207c
NabeelAkhtarHarl <66417858+NabeelAkhtarHarl@users.noreply.github.com>
2020-07-23 12:11:11 +0500

    simplecalculater cod

---
+++ calculater.py
@@ -0,0 +1,28 @@
+#functions
+def add(a,b):
+    return a+b
+def multi(a,b):
+    return a*b
+def sub(a,b):
+    return a-b
+def division(a,b):
+    return a/b
+
+#printshow
+print("select that operation you perform: \n 1.Add \n 2.multi \n 3.subtraction \n 4.division ")
+
+select=int(input("please select 1,2,3,4 "))
+a=int(input("please enter first number "))
+b=int(input("please enter second number "))
+ 
+if select==1:
+   print( add(a,b) )
+elif select==2:
+   print( multi(a,b) )
+elif select==3:
+   print( sub(a,b) )
+elif select==4:
+   print( division(a,b) )
+else:
+    print("invalid")
+    
